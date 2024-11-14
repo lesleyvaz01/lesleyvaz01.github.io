f00ce53286f0069debb9cf8da54f3a8d17a211d4
lesleyvaz01 <lesleyvaz01@gmail.com>
2024-11-14 14:51:45 -0700

    finishing up assignment 11

---
+++ js/assignment11.js
@@ -0,0 +1,39 @@
+let myData = {};
+
+function fetchData(){
+    
+    const comicNumber = Math.floor(Math.random() * 3000)+1;
+
+fetch(`https://corsproxy.io/?https://xkcd.com/${comicNumber}/info.0.json`)
+    .then(res =>{
+        if(res.ok){
+           // console.log(res.json());
+           return res.json();
+        
+        }
+        else{
+            console.log(res);
+        }
+        
+    }).then(res=> {
+        myData=res;
+        console.log(myData);
+        document.getElementById("comicTitle").innerHTML = myData.title;
+        document.getElementById("comicImg").src = myData.img;
+        document.getElementById("comicAltText").setAttribute("alt", myData.alt);
+        
+        let m = myData.month;
+        let d = myData.day;
+        let y = myData.year;
+        document.getElementById("comicDate").innerHTML = (m +'/'+ d +'/'+ y)
+    })
+    .catch(error => {console.log(error)})
+}
+fetchData();
+
+document.getElementById("generate").addEventListener("click", e=> {fetchData();})
+
+console.log(myData);
+
+// Returns a random integer from 0 to 99:
+//Math.floor(Math.random() * 3000);
